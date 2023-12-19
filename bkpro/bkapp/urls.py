feb6bd3b18fa26b9828ab457445325759d9501f7
mufx7 <mufyjig@gmail.com>
2023-12-19 16:58:47 +0530

    addeded

---
+++ bkpro/bkapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from bkapp import views, admin
+
+
+app_name='BankingApp'
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
+    path('loginsuccess/',views.loginsuccess,name='loginsuccess'),
+    path('last/',views.last,name='last'),
+    path('form/',views.form,name='form'),
+    path('check/',views.check,name='check')
+]
